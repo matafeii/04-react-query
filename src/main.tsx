612bcf6297dc200e7f09c4dee3656b2184ff1bb0
Kit_Kat <matafeii38@gmail.com>
2026-04-20 19:24:20 +0300

    npm install @tanstack/react-query-devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App/App";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ createRoot(root).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </StrictMode>,
 );
